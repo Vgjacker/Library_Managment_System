43518756dc616d9499cc634b83369a29e6c850e9
Vishal Gaurav <122996293+Vgjacker@users.noreply.github.com>
2025-10-03 08:40:06 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root"; // your MySQL username
+    private static final String PASSWORD = "root"; // your MySQL password
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
